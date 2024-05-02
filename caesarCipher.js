4b83c51ae085355b4a9c22d524f50a7719b3c7e9
wnnow <117201813+wnnow@users.noreply.github.com>
2024-05-02 21:45:55 +0700

    feat: Add decipher feature to caesarCipher
    - Implemented createDecipherShiftedArray function to generate the
        deciphered alphabet array based on the provided shift factor.
    - Modified caesarDeCipher function to utilize createDecipherShiftedArray
        for decryption.
    - Added test cases to ensure the correctness of the decryption
        functionality, covering scenarios with spaces and shift factors
        exceeding 26.

--- caesarCipher.js
+++ caesarCipher.js
@@ -29,6 +29,16 @@ const alphabet = [
 
 const upperCaseAlphabet = alphabet.map((ele) => ele.toUpperCase());
 
+//Cipher
+function createShiftedArray(shiftFactor) {
+  const shiftedArr = [];
+  for (let i = 0; i < 26; i++) {
+    shiftedArr.push(upperCaseAlphabet[shiftFactor % 26]);
+    shiftFactor++;
+  }
+  return shiftedArr;
+}
+
 function caesarCipher(str, shiftFactor) {
   const result = [];
   const strArr = str.split('');
@@ -40,10 +50,10 @@ function caesarCipher(str, shiftFactor) {
   //get index of each character from alphabet array
   //then push the same index from shiftedAlphabetArr
   strArr.forEach((ele) => {
-    if (ele === ' ') {
+    const index = alphabet.indexOf(ele);
+    if (index === -1) {
       result.push(ele);
     } else {
-      const index = alphabet.indexOf(ele);
       result.push(shiftedAlphabetArr[index]);
     }
   });
@@ -51,13 +61,39 @@ function caesarCipher(str, shiftFactor) {
   return result.join('');
 }
 
-function createShiftedArray(shiftFactor) {
+//Decipher
+function createDecipherShiftedArray(shiftFactor) {
   const shiftedArr = [];
+  let decipherShiftFactor = 26 - (shiftFactor % 26);
+
   for (let i = 0; i < 26; i++) {
-    shiftedArr.push(upperCaseAlphabet[shiftFactor % 26]);
-    shiftFactor++;
+    shiftedArr.push(alphabet[decipherShiftFactor % 26]);
+    decipherShiftFactor++;
   }
+
   return shiftedArr;
 }
 
-export { caesarCipher, alphabet };
+function caesarDeCipher(str, shiftFactor) {
+  const result = [];
+  const strArr = str.split('');
+
+  //create deciphershiftedAlphabetArray List
+  const decipherShiftedAlphabetArr = createDecipherShiftedArray(shiftFactor);
+
+  //if element equal to " " push element and skip to next one
+  //get index of each character from upperCaseAlphabet array
+  //then push the same index from decipherShiftedAlphabetArr
+  strArr.forEach((ele) => {
+    const index = upperCaseAlphabet.indexOf(ele);
+    if (index === -1) {
+      result.push(ele);
+    } else {
+      result.push(decipherShiftedAlphabetArr[index]);
+    }
+  });
+
+  return result.join('');
+}
+
+export { caesarCipher, caesarDeCipher };
--- caesarCipher.test.js
+++ caesarCipher.test.js
@@ -1,9 +1,41 @@
-import { caesarCipher } from './caesarCipher';
+import { caesarCipher, caesarDeCipher } from './caesarCipher';
 
-it('shift factor 1 enter "abc" expect "BCD"', () => {
+it('Encrypt shift factor 1 enter "abc" expect "BCD"', () => {
   expect(caesarCipher('abc', 1)).toMatch(/BCD/);
 });
 
-it('Work with space shift factor 1 enter "ab c" expect "BC D"', () => {
+it('Encrypt work with space shift factor 1 enter "ab c" expect "BC D"', () => {
   expect(caesarCipher('ab c', 1)).toMatch(/BC D/);
+});
+
+it('Encrypt work with space and shift factor exceed 26  enter "ab c" expect "BC D"', () => {
+  expect(caesarCipher('ab c', 27)).toMatch(/BC D/);
+});
+
+it('Encrypt work with space shift factor 1 enter "hello zehsco" expect "IFMMP AFITDP"', () => {
+  expect(caesarCipher('hello zehsco', 1)).toMatch(/IFMMP AFITDP/);
+});
+
+it('Encrypt wrapping from z to a with shift factor 3', () => {
+  expect(caesarCipher('xyz', 86)).toBe('FGH');
+});
+
+it('Encrypt preserving punctuation with shift factor 5', () => {
+  expect(caesarCipher('hello, world!', 5)).toBe('MJQQT, BTWQI!');
+});
+
+it('Decipher factor 1 enter "BCD" expect "abc"', () => {
+  expect(caesarDeCipher('BCD', 1)).toMatch(/abc/);
+});
+
+it('Decrypt work with space shift factor 1 enter "ab c" expect "BC D"', () => {
+  expect(caesarDeCipher('BC D', 1)).toMatch(/ab c/);
+});
+
+it('Decrypt work with space and shift factor exceed 26 enter "BC D" expect "ab c"', () => {
+  expect(caesarDeCipher('BC D', 27)).toMatch(/ab c/);
+});
+
+it('Decrypt work with space shift factor 23 enter "IFMMP AFITDP" expect "lipps dilwgs"', () => {
+  expect(caesarDeCipher('IFMMP AFITDP', 23)).toMatch(/lipps dilwgs/);
 });
